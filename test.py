ae399b4c4222a3d6f9810e9b14fdb1610aff7ea5
Vishnu Thiagarajan <Vishnu@Vishnus-MacBook-Air.local>
2016-10-23 09:19:56 -0500

    csv

--- test.py
+++ test.py
@@ -7,5 +7,8 @@ print jsontext
 idnum = jsontext["results"][0]["bioguide_id"]
 idnum = jsontext["results"][0]["govtrack_id"]
 print idnum
-r = requests.get("https://www.govtrack.us/api/v2/vote_voter/?person="+idnum+"&limit=1000&order_by=created&format=csv&fields=vote__id,created,option__value,vote__category,vote__chamber,vote__question,vote__number")
+#r = requests.get("https://www.govtrack.us/api/v2/vote_voter/?person="+idnum+"&limit=1000&order_by=created&format=csv&fields=vote__id,created,option__value,vote__category,vote__chamber,vote__question,vote__number")
+
+r = requests.get("https://www.govtrack.us/api/v2/vote_voter/?person=400222&limit=6000&order_by=-created&format=csv&fields=person__id,vote__id,option__value")
+
 print r.content
