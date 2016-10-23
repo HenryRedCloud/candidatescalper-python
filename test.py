be9ba38f95c970660efbc1b481444e92af619217
Vishnu Thiagarajan <Vishnu@Vishnus-MacBook-Air.local>
2016-10-23 09:03:18 -0500

    voting record

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import sys
+import requests
+import json
 
-print sys.argv
+r = requests.get("https://congress.api.sunlightfoundation.com/legislators?last_name=Williams&apikey=9b5f43862a344c20865c9491753c9f01") 
+jsontext = json.loads(r.content)
+print jsontext
+idnum = jsontext["results"][0]["bioguide_id"]
+idnum = jsontext["results"][0]["govtrack_id"]
+print idnum
+r = requests.get("https://www.govtrack.us/api/v2/vote_voter/?person="+idnum+"&limit=1000&order_by=created&format=csv&fields=vote__id,created,option__value,vote__category,vote__chamber,vote__question,vote__number")
+print r.content
